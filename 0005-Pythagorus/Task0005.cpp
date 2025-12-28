5ba9450f8cee7817faa7188d19a1db05d3af8c10
DevDewbie <speedfighteagle905@gmail.com>
2025-12-28 10:04:55 +0700

    Refactor main function to include return statement

--- 0005-Pythagorus/Task0005.cpp
+++ 0005-Pythagorus/Task0005.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-
-double a,b,c;
-cin >> a >> b;
-c = sqrt(pow(a,2)+pow(b,2));
-cout << fixed << setprecision(6) << c ;
+  double a,b,c;
+  cin >> a >> b;
+  c = sqrt(pow(a,2)+pow(b,2));
+  cout << fixed << setprecision(6) << c ;
+  return 0;
 }
