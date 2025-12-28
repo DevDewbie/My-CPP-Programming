17ad18bdffcd2be710aeba9fe1845a420bff8260
DevDewbie <speedfighteagle905@gmail.com>
2025-12-28 09:54:49 +0700

    0005-Pythagorus-28/12/2025

---
+++ 0005-Pythagorus/Task0005.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+double a,b,c;
+cin >> a >> b;
+c = sqrt(pow(a,2)+pow(b,2));
+cout << fixed << setprecision(6) << c ;
+}
