c32e3286acf9825011be55bd7ab62e1529309a3a
DevDewbie <speedfighteagle905@gmail.com>
2025-12-28 09:48:54 +0700

    0004-Character-Checker-28/12/2025

---
+++ 0004-Character-Checker/Task0004.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+	
+	string s;
+	bool u = false , l = false ;
+	cin >> s;
+	
+	for(char c:s) {
+		if(isupper(c))
+			u = true;
+		if(islower(c))
+			l = true;
+	}
+	if(u&&l)
+		cout << "Mix";
+	else if(u)
+		cout << "All Capital Letter";
+	else
+		cout << "All Small Letter";
+}
